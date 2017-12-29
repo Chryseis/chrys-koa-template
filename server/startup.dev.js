24105b582bf26224d3c8476e6fe0fec5d57b0f2b
allen <294931812@qq.com>
2017-12-29 19:20:42 +0800

    add package

--- server/startup.dev.js
+++ server/startup.dev.js
@@ -1,3 +1,20 @@
 /**
  * Created by Administrator on 2017/12/29.
  */
+const path=require('path');
+const Koa=require('koa');
+const Static=require('koa-static');
+const bodyParser=require('koa-bodyparser');
+
+
+
+const app=new Koa();
+
+app.use(async (ctx)=>{
+    ctx.body='hello world'
+})
+
+app.listen(3000,()=>{
+    console.log('listen at port 3000')
+})
+
