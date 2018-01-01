66eb9a7e1f67eea3d6230c78e8a62c166e6bb26a
allen <294931812@qq.com>
2018-01-02 01:49:55 +0800

    add prod config

--- server/bin/www.js
+++ server/bin/www.js
@@ -2,9 +2,18 @@
  * Created by Administrator on 2018/1/1.
  */
 const opn = require('opn');
-const app = require('../startup.dev');
+const devApp = require('../startup.dev');
+const prodApp = require('../startup.prod');
+const {devPort, prodProt} = require('../config').serverConfig
 
-app.listen(3000, () => {
-    console.log('app listening 3000...');
-    //opn('http://localhost:3000');
-})
+let env = process.env.NODE_ENV;
+if (env == 'production') {
+    prodApp.listen(prodProt, () => {
+        console.log(`app listening ${prodProt}...`);
+    })
+} else {
+    devApp.listen(devPort, () => {
+        console.log(`app listening ${devPort}...`);
+        //opn('http://localhost:3000');
+    })
+}
